7361e3f32bd79e97a0f8c6efdb51cf05512aa7ec
Adetunji Temidayo <adetunjitemidayo26@gmail.com>
2023-11-09 19:38:19 +0100

    Add project files.

---
+++ CatalogService/Database/DatabaseContext.cs
@@ -0,0 +1,10 @@
+﻿using CatalogService.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatalogService.Database
+{
+    public class DatabaseContext:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
